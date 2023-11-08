5f140290913010c7806d0cfa86bb4fbe5025ea85
jyrgen <jyrgen.vander@gmail.com>
2023-11-08 15:55:33 +0200

    ex 6 and 7

---
+++ yl_6.py
@@ -0,0 +1,11 @@
+a = int(input("insert a number: "))
+b = int(input("insert a number: "))
+c = int(input("insert a number: "))
+
+if a > b and a > c:
+    print("a: ", a)
+elif b > c:
+    print("b: ", b)
+else:
+    print("c: ", c)    
+
---
+++ yl_7.py
@@ -0,0 +1,5 @@
+a = int(input("enter a number: "))
+if a % 2:
+    print(a, "is odd")
+else:
+    print(a, "is even")
